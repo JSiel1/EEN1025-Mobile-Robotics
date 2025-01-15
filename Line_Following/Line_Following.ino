6078634e421a07230aa0c1b13c48c8fdeeba721f
JSiel1 <jakub.sielski2@mail.dcu.ie>
2025-01-15 16:58:16 +0000

    Update Line_Following.ino
    Added PID Calculation

--- Line_Following/Line_Following.ino
+++ Line_Following/Line_Following.ino
@@ -4,14 +4,25 @@ int motor1Phase = 38;
 int motor2PWM = 35;
 int motor2Phase = 36;
 
+//Function declerations
+void forward();
+void reverse();
+void left();
+void right();
+void stop();
+bool lineDetection(int sensorValue);  //true if line detected
+
+
 //Line Sensor Arrays
 int sensorArray[5] = {0 ,0 ,0 , 0, 0};
 int analogPins[5] = {4 , 5, 6, 7, 15};
 
+
 //Settings
 int speed = 150;
 bool run = true;
-int sensitivity = 500;
+
+int threshold = 800;
 
 //PID Settings
 float Kp = 1.0;   //Proportional Gain
@@ -23,7 +34,9 @@ float error = 0;
 float previousError = 0;
 float integral = 0;
 float derivative = 0;
-float correction = 0;
+float PID = 0;
+
+
 
 void setup(){
   Serial.begin(9600);
@@ -41,7 +54,12 @@ void setup(){
 }
 
 void loop(){
-  
+  //calculate PID error
+  error = (sensorArray[0] * -2) + (sensorArray[1] * -1) + (sensorArray[3] * 1) + (sensorArray[4] * 2)) / (sensorArray[0] + sensorArray[1] + sensorArray[3] + sensorArray[4] + 0.001);  
+  integral += error;
+  derrivative += errorr - previousError;
+  PID = (Kp * error) + (Ki * integral) + (Kd * derrivative);
+  previousError = error;
 }
 
 
@@ -85,4 +103,14 @@ void stop(){
   analogWrite(motor1PWM, 0);
   analogWrite(motor2PWM, 0);
   Serial.println("Stopping");
+}
+
+bool lineDetection(){
+  for (int n = 0; n < 5; n++){
+    if (analogRead(analogPins[n]) < threshold){
+      sensorArray[n] = 1;
+    } else {
+      sensorArray[n] = 0;
+    }
+  }
 }
