60afc732d6d64dce3744c462652ab774c150c6c2
JSiel1 <jakub.sielski2@mail.dcu.ie>
2025-01-16 10:50:29 +0000

    Create Line_following_test.ino

---
+++ Line_following_test/Line_following_test.ino
@@ -0,0 +1,99 @@
+//Motor Pins
+int motor1PWM = 37;
+int motor1Phase = 38;
+int motor2PWM = 35;
+int motor2Phase = 36;
+
+//Line Sensor Constants
+int sensorArray[5] = {0 ,0 ,0 , 0, 0};
+int analogPins[5] = {4 , 5, 6, 7, 15};
+
+//Settings
+int speed = 150;
+bool run = true;
+int whiteThreshold = 600;
+
+const float Kp = 0.5;       // Proportional control factor for damping
+
+int motor1Speed = 0;
+int motor2Speed = 0;
+
+void setup(){
+  Serial.begin(9600);
+  //Output pin Initialisation
+  pinMode(motor1Phase, OUTPUT);
+  pinMode(motor2Phase, OUTPUT);
+  pinMode(motor1PWM, OUTPUT);
+  pinMode(motor2PWM, OUTPUT);
+
+  //sensor Pin Initialisation
+  for (int i = 0; i < 6; i++){
+    pinMode(i, INPUT);
+  }
+}
+
+void loop(){
+    // Read sensor values
+  int leftValue = analogRead(analogPins[3]);
+  int leftOuterValue = analogRead(analogPins[4]);
+  int rightOuterValue = analogRead(analogPins[0]);
+  int rightValue = analogRead(analogPins[1]);
+
+  // Debugging output
+  //Serial.print("Left: ");
+  //Serial.print(leftValue);
+  //Serial.print(" | Left Outer: ");
+  //Serial.print(leftOuterValue);
+  //Serial.print(" | Right Outer: ");
+  //Serial.print(rightOuterValue);
+  //Serial.print(" | Right: ");
+  //Serial.println(rightValue);
+
+  Serial.print("Left: ");
+  Serial.print(motor1Speed);
+  Serial.print(" | Right: ");
+  Serial.println(motor2Speed);
+
+  // Calculate deviation
+  int deviation = 0;
+
+  if (leftOuterValue < whiteThreshold) {
+    deviation = -3;  // Strong left deviation
+  } else if (leftValue < whiteThreshold) {
+    deviation = -1;  // Mild left deviation
+  } else if (rightOuterValue < whiteThreshold) {
+    deviation = 3;   // Strong right deviation
+  } else if (rightValue < whiteThreshold) {
+    deviation = 1;   // Mild right deviation
+  }
+  else {
+    deviation = 0;
+  }
+
+  motor1Speed = speed - (deviation * Kp * speed);
+  motor2Speed = speed + (deviation * Kp * speed);
+
+  driveMotor();
+}
+
+void driveMotor(){
+  motor1Speed = constrain(motor1Speed, 0, 255);
+  motor2Speed = constrain(motor2Speed, 0, 255);
+
+  digitalWrite(motor1Phase, LOW);
+  digitalWrite(motor2Phase, LOW);
+  analogWrite(motor1PWM, motor1Speed);
+  analogWrite(motor2PWM, motor2Speed);
+}
+
+void stop(){
+  digitalWrite(motor1Phase, LOW);
+  digitalWrite(motor2Phase, LOW);
+  analogWrite(motor1PWM, 0);
+  analogWrite(motor2PWM, 0);
+  Serial.println("Stopping");
+}
+
+void calculateTurn(){
+}
+
