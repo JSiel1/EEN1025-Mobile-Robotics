61ba9054d2c81da53ae9526504fc0a859eede4fe
JSiel1 <jakub.sielski2@mail.dcu.ie>
2025-01-16 21:11:55 +0000

    Update Line_Following

--- Line_Following/Line_Following.ino
+++ Line_Following/Line_Following.ino
@@ -1,43 +1,24 @@
 //Motor Pins
-const int motor1PWM = 37;
-const int motor1Phase = 38;
-const int motor2PWM = 35;
-const int motor2Phase = 36;
+int motor1PWM = 37;
+int motor1Phase = 38;
+int motor2PWM = 35;
+int motor2Phase = 36;
 
-//Function declerations
-void forward();
-void reverse();
-void stop();
-void lineDetection();  
-void calculatePID();
-
-//Line Sensor Arrays
+//Line Sensor Constants
 int sensorArray[5] = {0 ,0 ,0 , 0, 0};
-const int analogPins[5] = {4 , 5, 6, 7, 15};
+int analogPins[5] = {4 , 5, 6, 7, 15};      //[0] is right most sensor
 
 //Settings
 int speed = 120;
-bool run = true;
+int whiteThreshold = 500;
 
-int whiteValue = 300;       //Sensor Value over white
-int darkValue = 2500;       //Sensor valye over Black
+const float Kp = 0.5;   //Lower is smoother  
 
-//PID Settings
-float Kp = 30.0;   //Proportional Gain
-float Ki = 0.0;   //Integral Gain
-float Kd = Kp*10;   //Derivative Gain
-
-//PID Variables
-float error = 0;
-float previousError = 0;
-float proportional = 0;
-float integral = 0;
-float derivative = 0;
-float PID = 0;
+int motor1Speed = 0;
+int motor2Speed = 0;
 
 void setup(){
   Serial.begin(9600);
-  
   //Output pin Initialisation
   pinMode(motor1Phase, OUTPUT);
   pinMode(motor2Phase, OUTPUT);
@@ -51,14 +32,19 @@ void setup(){
 }
 
 void loop(){
-  lineDetection();
-  calculatePID();
+  // Read sensor values
+  readSensors();
+  calculateTurn();
+
+  Serial.print("Left: ");
+  Serial.print(motor1Speed);
+  Serial.print(" | Right: ");
+  Serial.println(motor2Speed);
+
+  driveMotor();
 }
 
-void driveMotor(int turnSpeed){
-  int motor1Speed = speed - turnSpeed;    //Change sign if turning wrong direction
-  int motor2Speed = speed + turnSpeed;    //^^
-
+void driveMotor(){
   motor1Speed = constrain(motor1Speed, 0, 255);
   motor2Speed = constrain(motor2Speed, 0, 255);
 
@@ -66,13 +52,6 @@ void driveMotor(int turnSpeed){
   digitalWrite(motor2Phase, LOW);
   analogWrite(motor1PWM, motor1Speed);
   analogWrite(motor2PWM, motor2Speed);
-
-  Serial.print("PID Correction: \t Left Motor Speed: \t Right Motor Speed: ");
-  Serial.print(PID);
-  Serial.print("\t");
-  Serial.print(motor1Speed);
-  Serial.print("\t");
-  Serial.print(motor2Speed);
 }
 
 void stop(){
@@ -83,22 +62,56 @@ void stop(){
   Serial.println("Stopping");
 }
 
-void lineDetection(){
-  for (int n = 0; n < 5; n++){
-    sensorArray[n] = map(analogRead(analogPins[n]), whiteValue, darkValue, 0, 1000);    //Convert the range of sensor readings
-    sensorArray[n] = constrain(sensorArray[n], 0, 1000);    //Limit the possible sensor value
+void readSensors(){
+  for (int i = 0; i < 5; i++){
+    sensorArray[i] = analogRead(analogPins[i]);
   }
 }
 
-void calculatePID(){
-  //calculate PID error
-  error = ((sensorArray[0] * -2) + (sensorArray[1] * -1) + (sensorArray[3] * 1) + (sensorArray[4] * 2)) / (sensorArray[0] + sensorArray[1] + sensorArray[3] + sensorArray[4] + 1);  
+void calculateTurn(){
+  int deviation = 0;
+
+  if (detectNode()){
+    stop();
+    delay(1000);
+  }
+
+  if (sensorArray[4] < whiteThreshold) {
+    deviation = -3;  // Strong left deviation
+  } else if (sensorArray[3] < whiteThreshold) {
+    deviation = -1;  // Mild left deviation
+  } else if (sensorArray[0] < whiteThreshold) {
+    deviation = 3;   // Strong right deviation
+  } else if (sensorArray[1] < whiteThreshold) {
+    deviation = 1;   // Mild right deviation
+  }
+  else {
+    deviation = 0;
+  }
+
+  motor1Speed = speed - (deviation * Kp * speed);
+  motor2Speed = speed + (deviation * Kp * speed);
+}
+
+bool detectNode(){
+  int whiteCount = 0;
   
-  proportional = error;
-  integral += error;      //Integral Error
-  derivative = (error - previousError);    //Derivative Error
-  PID = (Kp * proportional) + (Ki * integral) + (Kd * derivative);
-  
-  previousError = error;
-  driveMotor(PID);
+  if(sensorArray[0] < whiteTheshold){
+    whiteCount++;
+  } 
+  if(sensorArray[1] < whiteThreshold){
+    whiteCount++;
+  } 
+  if(sensorArray[3] < whiteThreshold){
+    whiteCount++;
+  } 
+  if(sensorArray[4] < whiteThreshold){
+    whiteCount++;
+  }
+
+  if (whiteCount >= 3){
+    return true;
+  } else{
+    return false;
+  }
 }
