593d29c80048546f17f8323d05e9f380ecf7e259
JSiel1 <jakub.sielski2@mail.dcu.ie>
2025-01-29 20:04:38 +0000

    Update CloudServerCommunication.ino

--- CloudServerCommunication/CloudServerCommunication.ino
+++ CloudServerCommunication/CloudServerCommunication.ino
@@ -3,6 +3,9 @@
 
 #define BUFSIZE 512
 
+String inputString = ""; // Variable to store input string
+int receivedValue = 0;
+
 // Wi-Fi credentials
 const char *ssid = "iot";                // Replace with your Wi-Fi SSID
 const char *password = "manganese30sulphating"; // Replace with your Wi-Fi password
@@ -14,8 +17,7 @@ const char *teamID = "rhtr2655";      // Replace with your team's ID
 
 //Cloud Server Variables
 bool isRunning = false;
-bool resetReady = false; // New state to track if the robot is ready for a new destination
-int startingPosition = 0;  // Initial position of the robot
+int startingPosition = 2;  // Initial position of the robot
 int currentPosition = 0;   // Track the robot's current position
 String nextDestination = ""; // Store the next destination
 
@@ -33,20 +35,35 @@ void setup() {
   sendPosition(startingPosition);
 
   // Get next Position from server
-  nextDestination = receiveNextPosition();
-  if (nextDestination != "-1") {
-    Serial.print("Next Destination: ");
-    Serial.println(nextDestination);
-    isRunning = true; // Start the line-following process
-  } else {
-    Serial.println("Failed to get the next destination.");
-  }
+  //nextDestination = receiveNextPosition();
+  //if (nextDestination != "-1") {
+  //  Serial.print("Next Destination: ");
+  //  Serial.println(nextDestination);
+  //  isRunning = true; // Start the line-following process
+  //} else {
+  //  Serial.println("Failed to get the next destination.");
+  //}
 }
 
 void loop() {
-  if (isRunning) {
-
+  if (Serial.available() > 0) {  // Check if data is available to read
+        char receivedChar = Serial.read();
+        if (receivedChar == '\n') { // Check for end of input
+            if (inputString == "send") {
+                sendPosition(receivedValue);
+                Serial.println("Position sent.");
+                Serial.println(receiveNextPosition());
+            } else {
+                receivedValue = inputString.toInt();  // Convert input to integer
+                Serial.print("Updated value: ");
+                Serial.println(receivedValue);  // Print the updated value
+            }
+            inputString = ""; // Reset input string
+        } else {
+            inputString += receivedChar; // Append character to input string
+      }
   }
+
 
 }
 
@@ -66,7 +83,7 @@ void connectToWiFi() {
 
 // Function to send current position 
 void sendPosition(int position) {
-  if (client.connect(server, port)) {
+  if (client.connect(serverIP, serverPort)) {
     String postBody = "position=" + String(position);
 
     // Construct HTTP POST request
@@ -91,7 +108,7 @@ void sendPosition(int position) {
     client.stop();
   } else {
     Serial.println("Connection to server failed!");
-    client.stop()
+    client.stop();
   }
 }
 
@@ -120,12 +137,10 @@ String getResponseBody(String& response) {
 
 // Function to receive the next position
 int receiveNextPosition() {
-  if (client.connect(server, port)) {
     // Send a GET request to retrieve the next position
-    client.println("GET /api/next/" + String(teamID) + " HTTP/1.1");
-    client.println("Host: " + String(server));
-    client.println("Connection: close");
-    client.println();
+    //client.println("GET /api/next/" + String(teamID) + " HTTP/1.1");
+    //client.println("Connection: close");
+    //client.println();
 
     // Read and process the response
     String response = readResponse();
